a9f8109d934e1ed3bb82bd2a1027d234ba648665
Aryan Kumar <yankumar101@gmail.com>
2024-02-17 16:59:04 +0530

    day2.1

---
+++ ep2/epi2.js
@@ -0,0 +1,25 @@
+console.log("hello!");
+let a = 5
+let b =5
+console.log(a+b+4);
+let c = "hi"+5;
+console.log(typeof a, typeof b,
+    typeof c,);
+
+
+    let x ="aryan bhai";
+    let y = 22;
+    let z = 2.22;
+    const p = true;
+    let q = undefined;
+    let r = null;
+    console.log(x, y, z, p,q,r);
+    console.log(typeof x, typeof y, typeof z, typeof p,typeof q,typeof r);
+
+    const e = {
+"ji" : "hello"
+    }
+    e.ji= 100000;
+
+    console.log(e);
+

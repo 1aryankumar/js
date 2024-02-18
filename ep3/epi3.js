db6882c96bc649b15847ca18347ffe69e9e6cf61
Aryan Kumar <yankumar101@gmail.com>
2024-02-18 21:45:18 +0530

    day3

--- ep3/epi3.js
+++ ep3/epi3.js
@@ -1,1 +1,10 @@
 console.log("Starting the program");
+
+let age = 4;
+
+ if (age>18){
+     console.log("You are eligible to vote");
+ }
+ else if (age<18){
+     console.log("You are not eligible to vote");
+ }

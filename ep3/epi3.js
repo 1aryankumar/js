330782bbd72c84230714e34da1d957b39238625a
Aryan Kumar <yankumar101@gmail.com>
2024-02-20 21:55:35 +0530

    day5

--- ep3/epi3.js
+++ ep3/epi3.js
@@ -1,10 +1,20 @@
 console.log("Starting the program");
 
-let age = 4;
+let age = 16;
+let grace = 2;
 
- if (age>18){
+
+ if (age+grace>=18){
      console.log("You are eligible to vote");
  }
  else {
      console.log("You are not eligible to vote");
  }
+
+
+ console.log(age+grace);
+ console.log(age-grace);
+ console.log(age*grace);
+ console.log(a ge%grace);
+ console.log(age**grace);
+ console.log(age++);

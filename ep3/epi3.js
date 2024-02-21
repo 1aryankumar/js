3f86af8cfeb3e6010547bc227a8b56b78cd39e37
Aryan Kumar <yankumar101@gmail.com>
2024-02-21 21:49:03 +0530

    day5

--- ep3/epi3.js
+++ ep3/epi3.js
@@ -1,20 +1,38 @@
-console.log("Starting the program");
 
-let age = 16;
+let age = 0;
 let grace = 2;
 
 
  if (age+grace>=18){
      console.log("You are eligible to vote");
  }
+ else if (age == 0){
+    console.log("Are you kidding?");
+ }
  else {
      console.log("You are not eligible to vote");
  }
 
 
- console.log(age+grace);
- console.log(age-grace);
- console.log(age*grace);
- console.log(a ge%grace);
- console.log(age**grace);
- console.log(age++);
+//  console.log(age+grace);
+//  console.log(age-grace);
+//  console.log(age*grace);
+//  console.log(age%grace);
+//  console.log(age**grace);
+//  console.log(age++);
+
+
+let a=4;
+let b=2;
+
+let c = a>b?(a-b):(b-a);
+/* translates to :
+if a>b{
+    c = a-b;
+}
+else{
+    c = b-a;
+}
+*/
+
+console.log(c);

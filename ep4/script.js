be2829c3559db7f70bdf3e6374ab3de7458d5435
Aryan Kumar <yankumar101@gmail.com>
2024-02-25 13:48:05 +0530

    day8

--- ep4/script.js
+++ ep4/script.js
@@ -1,6 +1,19 @@
-let a = 1;
-console.log(a);
+// let a = 1;
+// console.log(a);
 
-for (let i = 0; i < 100; i++) {
-    console.log(a+i);
-}
+// for (let i = 0; i < 100; i++) {
+//     console.log(a+i);
+// }
+
+let obj = {
+    name: "aryan",
+    age: 21,
+    role: "admin",}
+
+    for (const key in obj) {
+       
+            const element = obj[key];
+            console.log(key,element);
+            
+        
+    }
